9c4a99ca668916b97bc64ee64fd8d242d59f750d
Serhat Köse <serhat.kose@vbt.com.tr>
2019-11-03 21:42:22 +0300

    edit file

--- dummyDatabase.js
+++ dummyDatabase.js
@@ -1,6 +1,6 @@
 let players = [
   { _id: 1, name: "Serhat", runs: "18426" },
   { _id: 2, name: "Ali", runs: "10500" },
-  { _id: 3, name: "Veli", runs: "10843" }
+  { _id: 3, name: "Ahmet", runs: "10843" }
 ];
 module.exports = players;
